bc6bc069899cca34274514aed0c558068d81a638
UmsrAkne <sibata486@gmail.com>
2026-08-22 23:00:37 +0900

    init

---
+++ MetadataBridge/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MetadataBridge.Utils;
+
+namespace MetadataBridge.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "MetadataBridge";
+
+        public MainWindowViewModel()
+        {
+            AppLogger.Info("MainWindowViewModel created");
+        }
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+    }
+}
