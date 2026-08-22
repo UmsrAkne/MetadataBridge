044f9346c1ba4e3e82c56aaa7d45febeb85fe708
UmsrAkne <sibata486@gmail.com>
2026-08-23 00:09:01 +0900

    Add functionality to modify "Seed" randomize value in exported JSON files
    - Introduced `ComfyWorkflowHelper` to update "randomize" to "fixed" for "Seed" nodes in JSON files.
    - Integrated `ComfyWorkflowHelper.FixSeedRandomize` in `ImageViewModel` after JSON export.
    - Added `Newtonsoft.Json` package dependency to facilitate JSON processing.

---
+++ MetadataBridge/Utils/ComfyWorkflowHelper.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MetadataBridge.Utils
+{
+    public static class ComfyWorkflowHelper
+    {
+        /// <summary>
+        /// 指定されたJSONファイル内の "title": "Seed" を持つノードの "randomize" を "fixed" に書き換えて上書き保存します。
+        /// </summary>
+        /// <param name="filePath">対象のJSONファイルパス</param>
+        /// <returns>変更されて保存された場合は true、対象が見つからなかった場合は false</returns>
+        public static bool FixSeedRandomize(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("ファイルが見つかりません。", filePath);
+            }
+
+            var jsonText = File.ReadAllText(filePath);
+            var jObj = JObject.Parse(jsonText);
+
+            // "title" が "Seed" のオブジェクトを検索
+            var seedNode = jObj.Descendants()
+                .OfType<JObject>()
+                .FirstOrDefault(o => (string)o["title"] == "Seed");
+
+            if (seedNode?["widgets_values"] is JArray widgetsArray)
+            {
+                var isUpdated = false;
+
+                for (var i = 0; i < widgetsArray.Count; i++)
+                {
+                    if ((string)widgetsArray[i] == "randomize")
+                    {
+                        widgetsArray[i] = "fixed";
+                        isUpdated = true;
+                    }
+                }
+
+                // 変更があった場合のみファイルに上書き保存
+                if (isUpdated)
+                {
+                    File.WriteAllText(filePath, jObj.ToString(Formatting.Indented));
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- MetadataBridge/ViewModels/ImageViewModel.cs
+++ MetadataBridge/ViewModels/ImageViewModel.cs
@@ -14,6 +14,8 @@ namespace MetadataBridge.ViewModels
             FileInfo = new FileInfo(info);
             var outputPath = JsonExtractor.ExportWorkflow(FileInfo.FullName);
             JsonFiles.Add(outputPath);
+            var success = ComfyWorkflowHelper.FixSeedRandomize(outputPath.FullName);
+            Console.WriteLine(success);
         }
 
         public FileInfo FileInfo { get => fileInfo; set => SetProperty(ref fileInfo, value); }
